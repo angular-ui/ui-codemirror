eb7f1c9c38e2741400b839775d0011d01503bcd5
AngularUI (via TravisCI) <angular-ui@googlegroups.com>
2013-10-30 06:34:22 +0000

    Travis commit : build 51

--- build/ui-codemirror.js
+++ build/ui-codemirror.js
@@ -91,7 +91,10 @@ angular.module('ui.codemirror', [])
             // Override the ngModelController $render method, which is what gets called when the model is updated.
             // This takes care of the synchronizing the codeMirror element with the underlying model, in the case that it is changed by something else.
             ngModel.$render = function () {
-              codeMirror.setValue(ngModel.$viewValue);
+              //Code mirror expects a string so make sure it gets one
+              //Although the formatter have already done this, it can be possible that another formatter returns undefined (for example the required directive)
+              var safeViewValue = ngModel.$viewValue || '';
+              codeMirror.setValue(safeViewValue);		
             };
   
             if (!ngModel.$viewValue){
