5cb84572a86f2b8d7c7444380849351dd031855e
Douglas Duteil <douglasduteil@gmail.com>
2013-10-31 14:08:14 +0100

    chore(karma): Update Karma version

--- test/karma.conf.js
+++ test/karma.conf.js
@@ -1,61 +1,72 @@
 // Karma configuration
-// Generated on Mon Apr 08 2013 22:58:24 GMT+0200 (CEST)
+// Generated on Thu Oct 31 2013 13:03:03 GMT+0100 (CET)
+
+module.exports = function(config) {
+  config.set({
+
+    // base path, that will be used to resolve files and exclude
+    basePath: '..',
 
 
-// base path, that will be used to resolve files and exclude
-basePath = '..';
+    // frameworks to use
+    frameworks: ['jasmine'],
 
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'bower_components/angular/angular.js',
-  'bower_components/angular-mocks/angular-mocks.js',
-  'bower_components/codemirror/lib/codemirror.js',
-  'ui-codemirror.js',
-  'test/*.spec.js'
-];
+    // list of files / patterns to load in the browser
+    files: [
+      'bower_components/angular/angular.js',
+      'bower_components/angular-mocks/angular-mocks.js',
+      'bower_components/codemirror/lib/codemirror.js',
+      'ui-codemirror.js',
+      'test/*.spec.js'
+    ],
 
 
-// list of files to exclude
-exclude = [
-  
-];
+    // list of files to exclude
+    exclude: [
+      
+    ],
 
 
-// test results reporter to use
-// possible values: 'dots', 'progress', 'junit'
-reporters = ['dots'];
-
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+    // test results reporter to use
+    // possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
+    reporters: ['dots'],
 
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+    // web server port
+    port: 9876,
 
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+    // enable / disable colors in the output (reporters and logs)
+    colors: true,
 
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome', 'Firefox', 'PhantomJS'];
+    // level of logging
+    // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
+    logLevel: config.LOG_INFO,
 
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 60000;
+    // enable / disable watching file and executing tests whenever any file changes
+    autoWatch: false,
 
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+    // Start these browsers, currently available:
+    // - Chrome
+    // - ChromeCanary
+    // - Firefox
+    // - Opera (has to be installed with `npm install karma-opera-launcher`)
+    // - Safari (only Mac; has to be installed with `npm install karma-safari-launcher`)
+    // - PhantomJS
+    // - IE (only Windows; has to be installed with `npm install karma-ie-launcher`)
+    browsers: ['Chrome', 'Firefox', 'PhantomJS'],
+
+
+    // If browser does not capture in given timeout [ms], kill it
+    captureTimeout: 60000,
+
+
+    // Continuous Integration mode
+    // if true, it capture browsers, run tests and exit
+    singleRun: false
+  });
+};
