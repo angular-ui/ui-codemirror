2643dc44cf0056fe7fa29eb40a436a717d0807b8
Rostyslav Diachok <infernalmaster1@gmail.com>
2014-12-05 23:19:59 +0200

    fix: refresh codemirror in next event loop

--- src/ui-codemirror.js
+++ src/ui-codemirror.js
@@ -5,7 +5,7 @@
  */
 angular.module('ui.codemirror', [])
   .constant('uiCodemirrorConfig', {})
-  .directive('uiCodemirror', ['uiCodemirrorConfig', function (uiCodemirrorConfig) {
+  .directive('uiCodemirror', ['uiCodemirrorConfig', '$timeout', function (uiCodemirrorConfig, $timeout) {
 
     return {
       restrict: 'EA',
@@ -98,7 +98,9 @@ angular.module('ui.codemirror', [])
             scope.$watch(iAttrs.uiRefresh, function (newVal, oldVal) {
               // Skip the initial watch firing
               if (newVal !== oldVal) {
-                codeMirror.refresh();
+                $timeout(function() {
+                  codeMirror.refresh();
+                });
               }
             });
           }
